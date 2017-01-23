a67885eb3db5fbaa0265cb066968ed19112c3158
Przemek <przemyslaw.dziadek@gmail.com>
2017-01-23 14:30:14 +0100

    remove bad Config_Interface class name

--- src/Reader/Php.php
+++ src/Reader/Php.php
@@ -1,17 +1,17 @@
 <?php
 namespace Wordpress_Framework\Config\v1\Reader;
 
-use  Wordpress_Framework\Config\v1\ConfigInterface;
+use  Wordpress_Framework\Config\v1\Config_Interface;
 use  Wordpress_Framework\Config\v1\Config;
 
 class Php implements Reader_Interface {
     /**
-     * Read from a file and create an Config object that implements ConfigInterface
+     * Read from a file and create an Config object that implements Config_Interface
      *
      * @param  string $filename
-     * @return Wordpress_Framework\Config\v1\ConfigInterface object that implements ConfigInterface
+     * @return Wordpress_Framework\Config\v1\Config_Interface object that implements Config_Interface
      */
-    public static function read_from_file(string $filename, bool $allowModifications = false): ConfigInterface {
+    public static function read_from_file(string $filename, bool $allowModifications = false): Config_Interface {
         if (!is_file($filename) || !is_readable($filename)) {
             throw new \RuntimeException(sprintf(
                 "File '%s' doesn't exist or not readable",
--- src/Reader/Reader_Interface.php
+++ src/Reader/Reader_Interface.php
@@ -1,14 +1,14 @@
 <?php
 namespace Wordpress_Framework\Config\v1\Reader;
 
-use  Wordpress_Framework\Config\v1\ConfigInterface;
+use  Wordpress_Framework\Config\v1\Config_Interface;
 
 interface Reader_Interface {
     /**
-     * Read from a file and create an Config object that implements ConfigInterface
+     * Read from a file and create an Config object that implements Config_Interface
      *
      * @param  string $filename
-     * @return Wordpress_Framework\Config\v1\ConfigInterface object that implements ConfigInterface
+     * @return Wordpress_Framework\Config\v1\Config_Interface object that implements Config_Interface
      */
-    public static function read_from_file(string $filename, bool $allowModifications = false): ConfigInterface;
+    public static function read_from_file(string $filename, bool $allowModifications = false): Config_Interface;
 }
